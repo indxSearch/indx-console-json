a40ef079423be28a1e06b2111dff0638ef7f85f5
Anders Tangen <anders@brutal.as>
2025-04-15 17:48:46 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace IndxConsoleAppJson
             // CREATE INSTANCE
             //
 
-            SearchEngineJson SearchEngine = new SearchEngineJson();
+            SearchEngineJson engine = new SearchEngineJson();
             // Load a license like this: new SearchEngineJson("file.license");
             // Get a developer license on https://indx.co
 
@@ -21,40 +21,40 @@ namespace IndxConsoleAppJson
             
             string file = "data/imdb_top10k.json";
             FileStream fstream = File.Open(file, FileMode.Open, FileAccess.Read);
-            SearchEngine.Init(fstream, out string _errorMessage);
+            engine.Init(fstream, out string _errorMessage);
             fstream.Close();
 
             //
             // SETUP FIELDS
             //
 
-            SearchEngine.GetField("Movie_Name")!.Indexable = true;
-            SearchEngine.GetField("Movie_Name")!.Weight = Weight.High;
-            SearchEngine.GetField("Stars")!.Indexable = true;
-            SearchEngine.GetField("Stars")!.Facetable = true;
-            SearchEngine.GetField("Stars")!.Weight = Weight.Low;
-            SearchEngine.GetField("Description")!.Indexable = true;
-            SearchEngine.GetField("Description")!.Weight = Weight.Med;
-            SearchEngine.GetField("Year_of_Release")!.Facetable = true;
+            engine.GetField("Movie_Name")!.Indexable = true;
+            engine.GetField("Movie_Name")!.Weight = Weight.High;
+            engine.GetField("Stars")!.Indexable = true;
+            engine.GetField("Stars")!.Facetable = true;
+            engine.GetField("Stars")!.Weight = Weight.Low;
+            engine.GetField("Description")!.Indexable = true;
+            engine.GetField("Description")!.Weight = Weight.Med;
+            engine.GetField("Year_of_Release")!.Facetable = true;
 
             //
             // LOAD DATA
             //
 
             fstream = File.Open(file, FileMode.Open, FileAccess.Read);
-            SearchEngine.LoadJson(fstream, out _);
+            engine.LoadJson(fstream, out _);
             fstream.Close();
 
             //
             // RUN INDEXING
             //
 
-            SearchEngine.Index();
+            engine.Index();
 
             // Check progress
-            while (SearchEngine.Status.SystemState != SystemState.Ready) // Print indexing progress
+            while (engine.Status.SystemState != SystemState.Ready) // Print indexing progress
             {
-                int progressPercent = SearchEngine.Status.IndexProgressPercent;
+                int progressPercent = engine.Status.IndexProgressPercent;
                 Console.Write($"\rIndexing {progressPercent}%");
                 Thread.Sleep(50); // check every 50ms
             }
@@ -78,13 +78,13 @@ namespace IndxConsoleAppJson
                 // SEARCH
                 //
 
-                var result = SearchEngine.Search(query);
+                var result = engine.Search(query);
                 if (result != null)
                 {
                     foreach(var rec in result.Records)
                     {
                         long key = rec.DocumentKey;
-                        string json = SearchEngine.GetJsonDataOfKey(key); // Get JSON object
+                        string json = engine.GetJsonDataOfKey(key); // Get JSON object
 
                         Console.WriteLine(json);
                     }
@@ -97,13 +97,13 @@ namespace IndxConsoleAppJson
                 // Enable facets in the query. This is by default set to false.
                 query.EnableFacets = true;
                 // Search again with facets enabled
-                result = SearchEngine.Search(query);
+                result = engine.Search(query);
 
                 // Set up KeyValuePair for facets
                 Dictionary<string, KeyValuePair<string, int>[]>? facets = result.Facets;
 
                 // Print facets
-                foreach (var field in SearchEngine.DocumentFields.GetFacetableFieldList())
+                foreach (var field in engine.DocumentFields.GetFacetableFieldList())
                 {
                     var fName = field.Name;
                     if (facets != null)
@@ -129,7 +129,7 @@ namespace IndxConsoleAppJson
                 //
 
                 Console.WriteLine($"\nExact hits found: {result.TruncationIndex + 1}"); // this will be 0 if query has large typos
-                Console.WriteLine("Version: " + SearchEngine.Status.Version);
+                Console.WriteLine("Version: " + engine.Status.Version);
 
                 // Continue prompt
                 Console.ForegroundColor = ConsoleColor.DarkBlue;
